8f4f6fa32947a401a7798ae2fce29e9741923829
sflwa <philip@southfloridawebadvisors.com>
2024-09-25 18:38:01 -0400

    Add files via upload

---
+++ astra-elementor-widget.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Astra Custom Meta Options for Elementor
+ * Description: Custom Elementor widget to modify Astra page options inside Elementor builder.
+ * Version: 1.0
+ * Author: Your Name
+ * 
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Load Elementor widget after Elementor is loaded
+function register_astra_meta_elementor_widget() {
+    // Ensure Elementor and Astra are active
+    if( did_action( 'elementor/loaded' ) && function_exists('astra_get_option') ) {
+        require_once( __DIR__ . '/widgets/astra-meta-options.php' );
+        \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Astra_Elementor_Meta_Widget() );
+    }
+}
+add_action( 'elementor/widgets/widgets_registered', 'register_astra_meta_elementor_widget' );
