bd6666fd68bbc1519b7b253b3e79e9c35e728188
sflwa <philip@southfloridawebadvisors.com>
2024-09-25 18:38:27 -0400

    Add files via upload

---
+++ widgets/astra-meta-options.php
@@ -0,0 +1,103 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Astra_Elementor_Meta_Widget extends \Elementor\Widget_Base {
+
+    public function get_name() {
+        return 'astra_meta_options';
+    }
+
+    public function get_title() {
+        return __( 'Astra Meta Options', 'astra-elementor' );
+    }
+
+    public function get_icon() {
+        return 'eicon-settings';
+    }
+
+    public function get_categories() {
+        return [ 'general' ];
+    }
+
+    protected function _register_controls() {
+
+        $this->start_controls_section(
+            'astra_options_section',
+            [
+                'label' => __( 'Astra Page Options', 'astra-elementor' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        // Add controls for Astra options here. For example:
+        $this->add_control(
+            'disable_header',
+            [
+                'label' => __( 'Disable Header', 'astra-elementor' ),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __( 'Yes', 'astra-elementor' ),
+                'label_off' => __( 'No', 'astra-elementor' ),
+                'return_value' => 'yes',
+                'default' => '',
+            ]
+        );
+
+        $this->add_control(
+            'disable_footer',
+            [
+                'label' => __( 'Disable Footer', 'astra-elementor' ),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __( 'Yes', 'astra-elementor' ),
+                'label_off' => __( 'No', 'astra-elementor' ),
+                'return_value' => 'yes',
+                'default' => '',
+            ]
+        );
+
+        $this->add_control(
+            'disable_title',
+            [
+                'label' => __( 'Disable Title', 'astra-elementor' ),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __( 'Yes', 'astra-elementor' ),
+                'label_off' => __( 'No', 'astra-elementor' ),
+                'return_value' => 'yes',
+                'default' => '',
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+
+        // Get post meta
+        $post_id = get_the_ID();
+
+        // Save Astra meta options for the page
+        update_post_meta( $post_id, 'ast-global-header-display', $settings['disable_header'] === 'yes' ? 'disabled' : '' );
+        update_post_meta( $post_id, 'astra-footer-display', $settings['disable_footer'] === 'yes' ? 'disabled' : '' );
+        update_post_meta( $post_id, 'site-post-title', $settings['disable_title'] === 'yes' ? 'disabled' : '' );
+
+        // Output content (optional, since we're working with meta fields only)
+      //  echo '<div class="astra-meta-options-widget">';
+      //  echo __( 'Astra page options have been updated.', 'astra-elementor' );
+      //  echo '</div>';
+    }
+
+    protected function _content_template() {
+        ?>
+        <#
+        view.addInlineEditingAttributes( 'disable_header', 'none' );
+        view.addInlineEditingAttributes( 'disable_footer', 'none' );
+        view.addInlineEditingAttributes( 'disable_title', 'none' );
+        #>
+        <div class="astra-meta-options-widget">
+            {{{ settings.disable_header === 'yes' ? '<?php _e( "Header Disabled", "astra-elementor" ); ?>' : '<?php _e( "Header Enabled", "astra-elementor" ); ?>' }}}
+            {{{ settings.disable_footer === 'yes' ? '<?php _e( "Footer Disabled", "astra-elementor" ); ?>' : '<?php _e( "Footer Enabled", "astra-elementor" ); ?>' }}}
+            {{{ settings.disable_footer === 'yes' ? '<?php _e( "Title Disabled", "astra-elementor" ); ?>' : '<?php _e( "Title Enabled", "astra-elementor" ); ?>' }}}
+        </div>
+        <?php
+    }
+}
